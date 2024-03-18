7211bf7d5a76e829e6e7099a94ef56a26ec5c927
zitske <eduardomontzitske@gmail.com>
2024-03-18 18:38:15 -0300

    update

--- lib/View/main_view.dart
+++ lib/View/main_view.dart
@@ -144,81 +144,81 @@ class _MyHomePageState extends State<MyHomePage> {
               })),
     ];
     final Controller c = Get.find();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Olá,'),
-                    Text(
-                      'Nome do usuário',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    )
-                  ]),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
+    return Obx(() => Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Olá,'),
+                        Text(
+                          'Nome do usuário',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ]),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Rampas'),
+                  leading: Icon(Icons.wheelchair_pickup),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Sair', style: TextStyle(color: Colors.red)),
+                  leading: Icon(
+                    Icons.exit_to_app,
+                    color: Colors.red,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+          body: status[c.index_tab.value],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: c.index_tab.value,
+            onTap: (index) {
+              c.index_tab.value = index;
+              c.update();
+            },
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.reviews_outlined),
+                label: 'Avaliação',
               ),
-            ),
-            ListTile(
-              title: Text('Rampas'),
-              leading: Icon(Icons.wheelchair_pickup),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Sair', style: TextStyle(color: Colors.red)),
-              leading: Icon(
-                Icons.exit_to_app,
-                color: Colors.red,
+              BottomNavigationBarItem(
+                icon: Icon(Icons.access_time),
+                label: 'Aprovação',
               ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: status[c.index_tab.value],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: c.index_tab.value,
-        onTap: (index) {
-          c.index_tab.value = index;
-          c.update();
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.reviews_outlined),
-            label: 'Avaliação',
+              BottomNavigationBarItem(
+                icon: Icon(Icons.check_circle),
+                label: 'Aprovadas',
+              ),
+            ],
+            selectedItemColor: Theme.of(context).colorScheme.primary,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.access_time),
-            label: 'Revisão',
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              adicionar();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check_circle),
-            label: 'Aprovadas',
-          ),
-        ],
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          adicionar();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ));
   }
 }
